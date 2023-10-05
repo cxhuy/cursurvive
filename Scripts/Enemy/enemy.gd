098d52392fa26445450e01d4d17da8e7d435326d
cxhuy <alvinkang1@gmail.com>
2023-10-05 03:31:25 -0500

    feat: add enemy rotation towards player

--- Scripts/Enemy/enemy.gd
+++ Scripts/Enemy/enemy.gd
@@ -1,14 +1,19 @@
 extends CharacterBody2D
 
+var player
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	player = get_node("../Player")
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+	
+	
+func _physics_process(delta):
+	look_at(player.global_position)
 	
 
 func _on_area_2d_body_entered(body):
