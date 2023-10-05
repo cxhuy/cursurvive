6094733fb6ab52985aaf86b10a8013f9969dd0f6
cxhuy <alvinkang1@gmail.com>
2023-10-05 03:37:23 -0500

    style: change player node to PascalCase

--- Scripts/Enemy/enemy.gd
+++ Scripts/Enemy/enemy.gd
@@ -1,10 +1,10 @@
 extends CharacterBody2D
 
-var player
+var Player
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	player = get_node("../Player")
+	Player = get_node("../Player")
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -13,10 +13,10 @@ func _process(delta):
 	
 	
 func _physics_process(delta):
-	if player != null:
-		look_at(player.global_position)
+	if Player != null:
+		look_at(Player.global_position)
 	
 
 func _on_area_2d_body_entered(body):
 	if body.name == "Player":
-		player.death()
+		Player.death()
