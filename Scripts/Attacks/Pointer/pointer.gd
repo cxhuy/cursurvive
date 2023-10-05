06467469b5d3b74b989ed111766759ba393eeb01
cxhuy <alvinkang1@gmail.com>
2023-10-05 06:14:37 -0500

    feat: add pointer fire

--- Scripts/Attacks/Pointer/pointer.gd
+++ Scripts/Attacks/Pointer/pointer.gd
@@ -1,7 +1,10 @@
 extends CharacterBody2D
 
+const SPEED = 50000
+
 var Player
 var offset = 65
+var fire = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -14,15 +17,25 @@ func _process(delta):
 	
 	
 func _physics_process(delta):
-	var mousePos = get_global_mouse_position()
-	var selfPos = self.global_position
-	if (abs(mousePos.x - selfPos.x) > 0.01):
-		self.global_position = Player.global_position
-		self.rotation = Player.get_node("Marker2D").rotation
-		self.rotation_degrees -= 25
-		self.global_position.x += cos(deg_to_rad(self.rotation_degrees - 90)) * offset
-		self.global_position.y += sin(deg_to_rad(self.rotation_degrees - 90)) * offset
+	if !fire:
+		self.scale *= 1.01
+		offset += 0.5
+		var mousePos = get_global_mouse_position()
+		var selfPos = self.global_position
+		if (abs(mousePos.x - selfPos.x) > 0.01):
+			self.global_position = Player.global_position
+			self.rotation = Player.get_node("Marker2D").rotation
+			self.rotation_degrees -= 25
+			self.global_position.x += cos(deg_to_rad(self.rotation_degrees - 90)) * offset
+			self.global_position.y += sin(deg_to_rad(self.rotation_degrees - 90)) * offset
+	else:
+		velocity = Vector2(cos(deg_to_rad(self.rotation_degrees - 90)), sin(deg_to_rad(self.rotation_degrees - 90))) * SPEED * delta
+		move_and_slide()
 		
 		
 func _on_area_2d_body_entered(body):
 	body.queue_free()
+
+
+func _on_timer_timeout():
+	fire = true
