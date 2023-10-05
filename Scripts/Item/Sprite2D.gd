dbd51da8d09061ba4d6dae661ec345dc6e7d4bb8
cxhuy <alvinkang1@gmail.com>
2023-10-05 12:15:19 -0500

    feat: add item random rotation

--- Scripts/Item/Sprite2D.gd
+++ Scripts/Item/Sprite2D.gd
@@ -3,6 +3,7 @@ extends Sprite2D
 const itemPaths = ["res://Sprites/Items/pointer_item.png"]
 
 var imagePath
+var rotationSpeed
 
 func _ready():
 	imagePath = itemPaths[get_node("..").itemId]
@@ -10,3 +11,8 @@ func _ready():
 		self.scale = Vector2(2, 2)		
 		var texture = load(imagePath)
 		self.texture = texture
+	rotationSpeed = randi_range(-5, 5) * 0.1
+		
+		
+func _process(delta):
+	rotation_degrees += rotationSpeed
