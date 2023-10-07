fe8fe5e8c15656f7c3942255872338113f4e1d8e
cxhuy <alvinkang1@gmail.com>
2023-10-07 17:13:47 -0500

    feat: clamp mine_sweeper position

--- Scripts/Attacks/Mine/mine_sweeper.gd
+++ Scripts/Attacks/Mine/mine_sweeper.gd
@@ -3,6 +3,15 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	var viewport_x = get_viewport_rect().size.x
+	var viewport_y = get_viewport_rect().size.y
+	var texture_width = get_node("MineSweeperSprite").texture.get_width()
+	var texture_height = get_node("MineSweeperSprite").texture.get_height()
+	self.global_position.x = \
+	clamp(self.global_position.x, texture_width / 2, viewport_x - texture_width / 2)
+	self.global_position.y = \
+	clamp(self.global_position.y, texture_height / 2, viewport_y - texture_height / 2)
+	print(get_node("MineSweeperSprite").texture.get_width())
 	var tween = get_tree().create_tween()
 	tween.tween_property(self, "scale", Vector2(1, 1), 0.2)	
 	await get_tree().create_timer(0.5).timeout
