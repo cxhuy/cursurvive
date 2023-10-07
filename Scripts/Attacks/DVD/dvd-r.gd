7a16280008f5410cd785f19a79ae9a2c99b75713
cxhuy <alvinkang1@gmail.com>
2023-10-07 02:57:51 -0500

    feat: add random disc image

--- Scripts/Attacks/DVD/dvd-r.gd
+++ Scripts/Attacks/DVD/dvd-r.gd
@@ -1,10 +1,12 @@
 extends Sprite2D
 
+var discNames = ["DVD-R", "DVD-RAM", "DVD-ROM", "DVD-RW"]
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
+	var discName = "res://Sprites/Attacks/DVD/" + discNames[randi_range(0, discNames.size() - 1)] + ".png"
+	var newTexture = load(discName)
+	texture = newTexture
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
