4f0d7a688e1ff8722287b8366c4f99bf2efa70c5
cxhuy <alvinkang1@gmail.com>
2023-10-08 02:17:40 -0500

    refactor: refactor mine scripts

--- Scripts/Attacks/Mine/mine.gd
+++ Scripts/Attacks/Mine/mine.gd
@@ -2,23 +2,20 @@ extends Node2D
 
 var canExplode = false
 
-# Called when the node enters the scene tree for the first time.
+
 func _ready():
-	var tween = get_tree().create_tween()
+	var tween = create_tween()
 	tween.tween_property(self, "scale", Vector2(1, 1), 0.2)	
 	await get_tree().create_timer(0.5).timeout
 	canExplode = true
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
-	
 
 func _on_area_2d_body_entered(body):
 	if canExplode:
 		get_node("AttackRange").queue_free()
 		get_node("MineSprite").queue_free()
-		var tween = get_tree().create_tween()
+		
+		var tween = create_tween()
 		tween.tween_property(get_node("AttackBody/CollisionShape2D"), "scale", Vector2(1, 1), 0.2)
 		await get_tree().create_timer(2).timeout
 		var tween2 = get_tree().create_tween()
