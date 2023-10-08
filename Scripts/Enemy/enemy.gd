e5dab583bb8f1b98eacd3fe4eef1082f9f8ed1b0
cxhuy <alvinkang1@gmail.com>
2023-10-07 23:18:27 -0500

    refactor: refactor enemy scripts

--- Scripts/Enemy/enemy.gd
+++ Scripts/Enemy/enemy.gd
@@ -2,34 +2,30 @@ extends CharacterBody2D
 
 const SPEED = 10000
 
-var Player
+@onready var Player = get_node("../../Player")
 var canMove = false
 
-# Called when the node enters the scene tree for the first time.
+
 func _ready():
-	var tween = get_tree().create_tween()
+	var tween = create_tween()
 	tween.tween_property(self, "scale", Vector2(1, 1), 0.3)	
-	Player = get_node("../../Player")
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
 	
 	
 func _physics_process(delta):
 	look_at(Player.global_position)	
+	
 	if canMove:
-		velocity = Vector2(cos(rotation), sin(rotation)) * SPEED * delta
+		self.velocity = Vector2(cos(self.rotation), sin(self.rotation)) * SPEED * delta
 		move_and_slide()
 	
 
 func _on_area_2d_body_entered(body):
-	if body.name == "Player":
+	if body.name == "Player": 
 		Player.death()
 
 
 func _on_timer_timeout():
+	# Enemy does not move or interact until timer is triggered
 	canMove = true
 	get_node("Area2D/CollisionShape2D").disabled = false
 	get_node("CollisionShape2D").disabled = false
