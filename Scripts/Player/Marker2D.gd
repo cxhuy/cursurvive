22fceff002681fccaebeab1567847e5e5a5b37ce
cxhuy <alvinkang1@gmail.com>
2023-10-07 22:36:37 -0500

    refactor: refactor player scripts

--- Scripts/Player/Marker2D.gd
+++ Scripts/Player/Marker2D.gd
@@ -1,15 +1,9 @@
 extends Marker2D
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	var mousePos = get_global_mouse_position()
-	var selfPos = self.global_position
-	if (abs(mousePos.x - selfPos.x) > 0.01):
+
+	if abs(mousePos.x - self.global_position.x) > 0.01: # Prevents player jittering
 		look_at(mousePos)
 		rotation_degrees += 105
--- Scripts/Player/player.gd
+++ Scripts/Player/player.gd
@@ -3,22 +3,10 @@ extends CharacterBody2D
 const SPEED = 400
 
 var mousePos
-var selfPos
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
-	
 
 func _physics_process(delta):
-	mousePos = get_global_mouse_position()
-	selfPos = self.global_position
-	self.velocity = (mousePos - selfPos) * SPEED * delta	
+	self.velocity = (get_global_mouse_position() - self.global_position) * SPEED * delta	
 	move_and_slide()
 	
 	
