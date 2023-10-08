011171c6e4ea310b45ff8c4c4051ad855d520cf4
cxhuy <alvinkang1@gmail.com>
2023-10-08 00:33:03 -0500

    refactor: refactor disc scripts

--- Scripts/Attacks/DVD/disc.gd
+++ Scripts/Attacks/DVD/disc.gd
@@ -1,26 +1,22 @@
 extends RigidBody2D
 
 const SPEED = 1000
+const DISC_SIZE = 0.2
 
 var velocity
-var discSize = 0.2
 
-# Called when the node enters the scene tree for the first time.
+
 func _ready():
-	get_node("CollisionShape2D").scale = Vector2(discSize, discSize)
-	get_node("Area2D").scale = Vector2(discSize, discSize)
-	get_node("Dvd-r").scale = Vector2(discSize, discSize)
+	get_node("CollisionShape2D").scale = Vector2(DISC_SIZE, DISC_SIZE)
+	get_node("Area2D").scale = Vector2(DISC_SIZE, DISC_SIZE)
+	get_node("DiscSprite").scale = Vector2(DISC_SIZE, DISC_SIZE)
 	var randRotation = randi_range(0, 360)
 	velocity = Vector2(cos(randRotation), sin(randRotation)) * SPEED
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
 	
 	
 func _physics_process(delta):
 	var collision = move_and_collide(velocity * delta)
+	
 	if collision:
 		velocity = velocity.bounce(collision.get_normal())
 		
--- Scripts/Attacks/DVD/dvd-r.gd
+++ Scripts/Attacks/DVD/dvd-r.gd
@@ -2,12 +2,13 @@ extends Sprite2D
 
 var discNames = ["DVD-R", "DVD-RAM", "DVD-ROM", "DVD-RW"]
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	var discName = "res://Sprites/Attacks/DVD/" + discNames[randi_range(0, discNames.size() - 1)] + ".png"
-	var newTexture = load(discName)
-	texture = newTexture
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _ready():
+	var discName = \
+	"res://Sprites/Attacks/DVD/" + discNames[randi_range(0, discNames.size() - 1)] + ".png"
+	var newTexture = load(discName)
+	self.texture = newTexture
+
+
 func _process(delta):
 	self.rotation_degrees += 10
