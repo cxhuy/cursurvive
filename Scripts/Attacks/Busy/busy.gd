1678293bc4d61897295c3e60db195b560dfd8b46
cxhuy <alvinkang1@gmail.com>
2023-10-08 01:58:10 -0500

    refactor: refactor busy scripts

--- Scripts/Attacks/Busy/busy.gd
+++ Scripts/Attacks/Busy/busy.gd
@@ -4,21 +4,12 @@ const SPEED = 400
 
 var enemy_to_chase
 var chase = false
-var enemyPos
-var selfPos
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	var enemies_in_range = get_node("AttackRange").get_overlapping_areas()
-	
-	if chase and enemy_to_chase != null:
-		pass
-	else:
+
+	if !chase or enemy_to_chase == null:
 		if enemies_in_range.size() != 0:
 			enemy_to_chase = enemies_in_range[randi_range(0, enemies_in_range.size() - 1)]
 			chase = true
@@ -30,9 +21,7 @@ func _process(delta):
 			
 func _physics_process(delta):
 	if chase and enemy_to_chase != null:
-		enemyPos = enemy_to_chase.global_position
-		selfPos = self.global_position
-		self.velocity = (enemyPos - selfPos) * SPEED * delta
+		self.velocity = (enemy_to_chase.global_position - self.global_position) * SPEED * delta
 		move_and_slide()
 		
 		
@@ -41,7 +30,7 @@ func _on_attack_body_body_entered(body):
 	
 
 func _on_timer_timeout():
-	var tween = get_tree().create_tween()
+	var tween = create_tween()
 	tween.tween_property(self, "scale", Vector2(0, 0), 0.1)
 	await get_tree().create_timer(0.1).timeout
 	self.queue_free()
