97a0566d79c24ba10577ce771ddec6f111568e72
cxhuy <alvinkang1@gmail.com>
2023-10-09 00:16:50 -0500

    refactor: refactor enemySpawn script

--- Scripts/Enemy/enemySpawn.gd
+++ Scripts/Enemy/enemySpawn.gd
@@ -11,51 +11,56 @@ var patternTime = randi_range(5, 10)
 
 
 func _on_timer_timeout():
-	var spawnedEnemy = enemy.instantiate()
-	var spawnPos = Vector2.ZERO
+	var enemyToSpawn = enemy.instantiate()
+	var enemySpawnPos = Vector2.ZERO
 	var validSpawn = false
 	
 	while !validSpawn:
-		spawnPos.x = randi_range(0 + 50, get_viewport_rect().size.x - 50)
-		spawnPos.y = randi_range(0 + 50, get_viewport_rect().size.y - 50)
-		if SPAWN_LIMIT < spawnPos.distance_to(Player.global_position) and \
-		spawnPos.distance_to(Player.global_position) < SPAWN_RADIUS:
+		enemySpawnPos.x = randi_range(0 + 50, get_viewport_rect().size.x - 50)
+		enemySpawnPos.y = randi_range(0 + 50, get_viewport_rect().size.y - 50)
+		
+		if SPAWN_LIMIT < enemySpawnPos.distance_to(Player.global_position) and \
+		enemySpawnPos.distance_to(Player.global_position) < SPAWN_RADIUS:
 			validSpawn = true
 
-	spawnedEnemy.position = spawnPos
-	add_child(spawnedEnemy)
+	enemyToSpawn.global_position = enemySpawnPos
+	add_child(enemyToSpawn)
 	
 	if timeCount == patternTime:
 		spawn_enemy_pattern()
 		timeCount = 0
 		patternTime = randi_range(5, 10)
+		
 	else:
 		timeCount += 1
 		
 
 func spawn_enemy_pattern():
-	var pattern_id = randi_range(0, 0)
-	match pattern_id:
+	var patternId = randi_range(0, 0)
+	
+	match patternId:
 		0: # Circle of enemies
 			const enemyCountVars = [12, 18, 24, 30, 36]
 			
 			var enemyCount = enemyCountVars[randi() % enemyCountVars.size()]
-			var radius = enemyCount * 5
-			var centerPos = Vector2.ZERO
-			var spawnPos = Vector2.ZERO
+			var circleRadius = enemyCount * 5
+			var circleCenterPos = Vector2.ZERO
+			var enemySpawnPos = Vector2.ZERO
 			var validSpawn = false
-			var spawnedEnemy
+			var enemyToSpawn
 			
 			while !validSpawn:
-				centerPos.x = randi_range(0 + 50, get_viewport_rect().size.x - 50)
-				centerPos.y = randi_range(0 + 50, get_viewport_rect().size.y - 50)
-				if SPAWN_LIMIT < centerPos.distance_to(Player.global_position) and \
-				centerPos.distance_to(Player.global_position) < SPAWN_RADIUS:
+				circleCenterPos.x = randi_range(0 + 50, get_viewport_rect().size.x - 50)
+				circleCenterPos.y = randi_range(0 + 50, get_viewport_rect().size.y - 50)
+				if SPAWN_LIMIT < circleCenterPos.distance_to(Player.global_position) and \
+				circleCenterPos.distance_to(Player.global_position) < SPAWN_RADIUS:
 					validSpawn = true
 					
 			for i in range(enemyCount):
-				spawnedEnemy = enemy.instantiate()
-				spawnPos.x = centerPos.x + sin(deg_to_rad(i * 360 / enemyCount)) * radius
-				spawnPos.y = centerPos.y + cos(deg_to_rad(i * 360 / enemyCount)) * radius
-				spawnedEnemy.global_position = spawnPos
-				add_child(spawnedEnemy)
+				enemyToSpawn = enemy.instantiate()
+				enemySpawnPos.x = \
+					circleCenterPos.x + sin(deg_to_rad(i * 360 / enemyCount)) * circleRadius
+				enemySpawnPos.y = \
+					circleCenterPos.y + cos(deg_to_rad(i * 360 / enemyCount)) * circleRadius
+				enemyToSpawn.global_position = enemySpawnPos
+				add_child(enemyToSpawn)
