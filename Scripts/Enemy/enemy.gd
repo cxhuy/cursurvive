3019e8c4c19798237c06feb842a0df6d50d70f46
cxhuy <alvinkang1@gmail.com>
2023-10-10 22:57:25 -0500

    feat: free !followPlayer enemy when exit screen

--- Scripts/Enemy/enemy.gd
+++ Scripts/Enemy/enemy.gd
@@ -32,3 +32,8 @@ func _on_area_2d_body_entered(body):
 func _on_timer_timeout():
 	# Enemy does not move or interact until timer is triggered
 	canMove = true
+
+
+func _on_visible_on_screen_notifier_2d_screen_exited():
+	if !followPlayer:
+		self.queue_free()
