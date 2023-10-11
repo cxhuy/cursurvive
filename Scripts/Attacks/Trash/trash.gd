0a6685308b7d2b7d42777f400cabdf6775760e30
cxhuy <alvinkang1@gmail.com>
2023-10-11 17:07:43 -0500

    feat: add trash attack delay

--- Scripts/Attacks/Trash/trash.gd
+++ Scripts/Attacks/Trash/trash.gd
@@ -2,19 +2,21 @@ extends Node2D
 
 const power = 175
 
+var canAttack = false
 var enemies_in_range
 
 
 func _ready():
-	pass
-
+	await get_tree().create_timer(1).timeout
+	canAttack = true
 
 func _process(delta):
-	enemies_in_range = get_node("GravityRange").get_overlapping_areas()
-	for enemy in enemies_in_range:	
-		enemy = enemy.get_node("../")
-		enemy.global_position -= \
-		(enemy.global_position - self.global_position).normalized() * delta * power
+	if canAttack:
+		enemies_in_range = get_node("GravityRange").get_overlapping_areas()
+		for enemy in enemies_in_range:	
+			enemy = enemy.get_node("../")
+			enemy.global_position -= \
+			(enemy.global_position - self.global_position).normalized() * delta * power
 
 
 func _on_attack_range_body_entered(body):
