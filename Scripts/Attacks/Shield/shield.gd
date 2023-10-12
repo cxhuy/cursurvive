d325a5869d467ba8fec5ad2f592d6742aa085dce
cxhuy <alvinkang1@gmail.com>
2023-10-11 19:15:12 -0500

    feat: add shield sprite texture script

--- Scripts/Attacks/Shield/shield.gd
+++ Scripts/Attacks/Shield/shield.gd
@@ -1,5 +1,13 @@
 extends Node2D
 
+var shieldNames = ["red", "green", "blue", "yellow"]
+var shieldId
+
+
+func _ready():
+	get_node("ShieldSprite").texture = \
+	load("res://Sprites/Attacks/Shield/shield_" + shieldNames[shieldId] + ".png")
+
 
 func _on_attack_range_body_entered(body):
 	body.queue_free()
