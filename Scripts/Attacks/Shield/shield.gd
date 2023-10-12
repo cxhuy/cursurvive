44725c30bdc6d752357417d102c1074609c2c9ed
cxhuy <alvinkang1@gmail.com>
2023-10-11 19:32:46 -0500

    feat: add shield animation

--- Scripts/Attacks/Shield/shield.gd
+++ Scripts/Attacks/Shield/shield.gd
@@ -5,6 +5,9 @@ var shieldId
 
 
 func _ready():
+	var tween = create_tween()
+	tween.tween_property(self, "scale", Vector2(1.2, 1.2), 0.2)
+	
 	get_node("ShieldSprite").texture = \
 	load("res://Sprites/Attacks/Shield/shield_" + shieldNames[shieldId] + ".png")
 
@@ -15,3 +18,9 @@ func _physics_process(delta):
 
 func _on_attack_range_body_entered(body):
 	body.queue_free()
+	
+
+func _on_duration_timeout():
+	var tween = create_tween()
+	tween.tween_property(self, "scale", Vector2(0, 0), 0.1)
+	await get_tree().create_timer(0.1).timeout
