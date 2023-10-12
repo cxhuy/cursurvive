3c24f18df706830a94c2e40fc483ef582c9a9c89
cxhuy <alvinkang1@gmail.com>
2023-10-12 00:48:25 -0500

    feat: add firewall scene

--- Scripts/Item/item.gd
+++ Scripts/Item/item.gd
@@ -8,6 +8,7 @@ var busy = preload("res://Scenes/Attacks/busy.tscn")
 var mine = preload("res://Scenes/Attacks/mine_sweeper.tscn")
 var trash = preload("res://Scenes/Attacks/trash.tscn")
 var shields = preload("res://Scenes/Attacks/shields.tscn")
+var firewall = preload("res://Scenes/Attacks/firewall.tscn")
 
 var itemId
 var velocity = Vector2(randi_range(-50, 50), randi_range(-50, 50))
@@ -50,7 +51,8 @@ func _on_area_2d_body_entered(body):
 				itemToUse.global_position = self.global_position
 				
 			6: # Firewall attack
-				pass
+				itemToUse = firewall.instantiate()
+				itemToUse.global_position = self.global_position
 	
 	Game.call_deferred("add_child", itemToUse)
 	Game.get_node("Items").addItem()
