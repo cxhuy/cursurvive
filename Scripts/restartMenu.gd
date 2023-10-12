1b91d5827ffe2c026c2cb66c0f92d069f7260e60
cxhuy <alvinkang1@gmail.com>
2023-10-12 18:05:13 -0500

    feat: add game clear

--- Scripts/restartMenu.gd
+++ Scripts/restartMenu.gd
@@ -7,6 +7,12 @@ func populateMenu():
 	Game.totalKillCount = 0
 
 
+func populateMenuClear():
+	$Label.text = "Congratulations! You survived for 100 seconds!\n" + \
+	"and killed a total of " + str(Game.totalKillCount) + " enemies"
+	Game.totalKillCount = 0
+
+
 func restartGame():
 	$".".hide
 	get_tree().reload_current_scene()
--- Scripts/time.gd
+++ Scripts/time.gd
@@ -3,6 +3,7 @@ extends Label
 var startTime
 var currentTimeSec
 
+
 func _ready():
 	startTime = Time.get_ticks_msec()
 
@@ -11,3 +12,7 @@ func _process(delta):
 	Game.survivedTime = (Time.get_ticks_msec() - startTime)	
 	currentTimeSec = (Time.get_ticks_msec() - startTime) / 1000
 	self.text = str(100 - currentTimeSec)
+	if 100 - currentTimeSec <= 0:
+		get_tree().paused = true
+		get_node("../RestartMenu").populateMenuClear()
+		get_node("../RestartMenu").show()
