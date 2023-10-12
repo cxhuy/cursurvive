4b925ea9397c2d9d30edf0f96a06c96d2ec7c674
cxhuy <alvinkang1@gmail.com>
2023-10-12 10:43:57 -0500

    fix: fix timer not resetting when scene restart

--- Scripts/time.gd
+++ Scripts/time.gd
@@ -1,6 +1,12 @@
 extends Label
 
+var startTime
+
+
+func _ready():
+	startTime = Time.get_ticks_msec()
+
 
 func _process(delta):
-	var currentTimeSec = Time.get_ticks_msec() / 1000
+	var currentTimeSec = (Time.get_ticks_msec() - startTime) / 1000
 	text = str(100 - currentTimeSec)
