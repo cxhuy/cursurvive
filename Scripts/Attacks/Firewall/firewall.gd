9e69324e00684af5533663187a6dd26d1dc5d367
cxhuy <alvinkang1@gmail.com>
2023-10-12 01:28:25 -0500

    feat: add firewall wall spawn

--- Scripts/Attacks/Firewall/firewall.gd
+++ Scripts/Attacks/Firewall/firewall.gd
@@ -1,8 +1,13 @@
 extends RigidBody2D
 
 const friction = 0.97
+const wallCount = 60
 
+var wall = preload("res://Scenes/Attacks/wall.tscn")
+var wallInstance
 var velocity
+var wallSpawned = 0
+
 
 func _physics_process(delta):
 	var collision = move_and_collide(velocity * delta)
@@ -11,3 +16,10 @@ func _physics_process(delta):
 		velocity = velocity.bounce(collision.get_normal())
 	velocity *= friction
 	
+
+func _on_wall_spawn_timeout():
+	if wallSpawned < wallCount:
+		wallInstance = wall.instantiate()
+		wallInstance.global_position = self.global_position
+		get_node("../..").add_child(wallInstance)
+		wallSpawned += 1
